d85d2a248a009dc95418c2c3388828b3dfa44ee4
Guru <guru85111@gmail.com>
2024-09-10 10:18:05 +0530

    "Added aos library, updated Header component with animations, and made various CSS changes to layout and styling."

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import HeaderCss from "./Header.module.css";
 import logo from "../../assets/logo.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
@@ -12,23 +14,40 @@ const Header = () => {
 
   const removeActive = (path) => {
     setIsActive(false);
-      navigate(path);
-      window.scrollTo(0, 0);
-  }
-  
+    navigate(path);
+    window.scrollTo(0, 0);
+  };
+
   const navigate = useNavigate();
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className={HeaderCss.head}>
-      <nav className={HeaderCss.navbar} >
-        <img src={logo} alt="logo" onClick={() => {navigate('/'); window.scrollTo(0, 0)}} />
+    <div data-aos='fade-down' className={HeaderCss.head}>
+      <nav className={HeaderCss.navbar}>
+        <img
+          src={logo}
+          alt="logo"
+          onClick={() => {
+            navigate("/");
+            window.scrollTo(0, 0);
+          }}
+        />
         <div className={HeaderCss.listmob}>
-            <ul className={`${HeaderCss.list} ${isActive ? HeaderCss.active : ""}`}>
-            <li onClick={() => removeActive('/')}>Home</li>
-            <li onClick={() => removeActive('/products')}>Products</li>
-            <li onClick={() => removeActive('/career')}>Career</li>
-            <li onClick={() => removeActive('/contact')}>Contact</li>
+          <div className={HeaderCss.mobstyle}>
+            <ul
+              className={`${HeaderCss.list} ${isActive ? HeaderCss.active : ""}`}>
+              <li onClick={() => removeActive("/")}>Home</li>
+              <li onClick={() => removeActive("/products")}>Products</li>
+              <li onClick={() => removeActive("/career")}>Career</li>
+              <li onClick={() => removeActive("/contact")}>Contact us</li>
             </ul>
+          </div>
         </div>
         <div
           className={`${HeaderCss.hamburger} ${
